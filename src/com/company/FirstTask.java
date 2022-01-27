9121b3f4c675646cd2b4cc51a39ec33819f416e4
Yigit Mustafa Gunes <yigit.gnes@gmail.com>
2022-01-27 09:54:45 +0300

    revise

--- src/com/company/FirstTask.java
+++ src/com/company/FirstTask.java
@@ -14,6 +14,6 @@ public class FirstTask {
     }
     public static void main(String[] args) {
 
-         System.out.println("First Task Answer is : "+ firstTask(10));
+         System.out.println("First Task Answer is : "+ firstTask(100));
     }
 }
